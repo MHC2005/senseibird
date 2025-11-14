a6b88dd516869d1ed80f91ae71e93d20e12df74e
Mateo Hernández <mhc@MacBook-Air-de-Mateo.local>
2025-11-14 15:12:07 -0300

    contenedores de back y front

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
+
+  experimental: {
+    appDir: true,
+    serverMinification: false, // evita romper CSS
+  },
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
